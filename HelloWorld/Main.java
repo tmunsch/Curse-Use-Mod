0262e89ea221f13acaab61a2171a4528412888a4
TeddyTuned <ted@munschnetwork.com>
2022-05-26 23:18:10 -0400

    Succesful Build

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,12 +1,28 @@
 package HelloWorld;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("****************************************************");
-        System.out.println("Your Server is Installed");
-        System.out.println("Now Select a Modpack at the Top and click install");
-        System.out.println("****************************************************");
+public class Main {
+    public static void main(String args[])
+        throws InterruptedException {
+        String importantInfo[] = {
+            "**********************************************",
+            "*************** SERVER INSTALLED *************",
+            "**********************************************",
+            "************** SELECT YOUR MODPACK ***********",
+            "*************** CLICK INSTALL ****************",
+            "**********************************************",
+            "************ Copyright Bofanodes.io **********",
+            "**********************************************"
+        };
+
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            //Pause for 10 seconds
+            Thread.sleep(500);
+            //Print a message
+            System.out.println(importantInfo[i]);
+        }
+        try{System.in.read();}
+catch(Exception e){}
     }
 }
